8b99915a0baba842d99283498ae5a261eb7ec171
ShishantG87 <shishantbohora44@gmail.com>
2025-03-19 00:29:33 -0400

    Co-authored-by: Quazberry <Quazberry@users.noreply.github.com>

---
+++ fight_prob.py
@@ -0,0 +1,8 @@
+# Import Files
+import fight_values
+
+class FightProb:
+    pass
+
+    
+# Open file and create variables to read in information from UFC Dataset
---
+++ fight_values.py
@@ -0,0 +1,8 @@
+# Create Functions that will read and hold values
+# Declare variables
+class Fightvalues:
+    def fighter (first, last, nickname, height, weight,
+             reach, stance, win, lose, draw):
+        {
+
+        }

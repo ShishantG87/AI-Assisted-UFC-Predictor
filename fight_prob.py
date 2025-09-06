433151c29ab9f360682191873530137b9bb73781
Quazberry <nathanq619@icloud.com>
2025-09-05 20:56:40 -0700

    Update 2.1 Modified UFC API

--- fight_prob.py
+++ fight_prob.py
@@ -1,9 +1,3 @@
-# Import Files
-import fight_values
+from ufc import get_event
 
-class FightProb:
-
-# Open file and create variables to read in information from UFC Dataset
-    with open(r"second-data-set\ufc_fighter_stats.txt", "r") as fdata:
-        for line in fdata:
-            print(line.rstrip())
+print(get_event('UFC-290'))

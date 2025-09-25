371f2c0d85dbb7cd6f6b36609d386b624723b7f0
ShishantG87 <shishantbohora44@gmail.com>
2025-09-25 16:59:17 -0400

    yo

---
+++ test.py
@@ -0,0 +1,16 @@
+from ai21 import AI21Client
+from ai21.models.chat import ChatMessage
+
+messages = [
+    ChatMessage(role="user", content="Hello how are you?"),
+]
+
+#0b8015db-eda7-4568-9083-ae44e05308e3 = api key lil bro
+client = AI21Client()
+
+response = client.chat.completions.create(
+    messages=messages,
+    model="jamba-large",
+    max_tokens=1024,
+)
+print(response[0])
